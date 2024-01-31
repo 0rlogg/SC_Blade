555c4fa8e994f79d1a94a626b8849637632ecc18
Daniel Miranda Barcelona <73300841+0rlogg@users.noreply.github.com>
2024-01-31 20:41:06 +0100

    Create Client_Side.py

---
+++ Client_Side.py
@@ -0,0 +1,54 @@
+import socket
+from pynput import keyboard as pynput_keyboard
+
+# Configura la dirección IP y el puerto del PC secundario
+SERVER_IP = '192.168.1.139'  # Reemplaza con la dirección IP del PC secundario
+SERVER_PORT = 55555  # Elige un puerto disponible
+
+ALLOWED_KEYS = {'e', '0', 't', '1', '5'}  # Lista de teclas permitidas
+
+def send_command(command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((SERVER_IP, SERVER_PORT))
+        s.sendall(command.encode('utf-8'))
+
+def simulate_key_press_release(key):
+    try:
+        with pynput_keyboard.Controller() as controller:
+            controller.press(key)
+            controller.release(key)
+    except Exception as e:
+        print(f"Error al simular la pulsación de tecla: {e}")
+
+# Define las teclas que activarán los comandos
+encender_puesto = 'e'  # e
+disparar = '0'  # CLICK IZQUIERDO
+target = 't'  # t
+MM = '1'  #  # mode change and mining or savaging M -> CLICK DERECHO -> CLICK IZQUIERDO
+center_turret = '5'
+
+def on_press(key):
+    try:
+        key_char = key.char
+    except AttributeError:
+        key_char = str(key)
+
+    if key_char in ALLOWED_KEYS:
+        if key_char == encender_puesto:
+            send_command('encendido')
+        elif key_char == disparar:
+            send_command('disparar')
+        elif key_char == target:
+            send_command('target')
+        elif key_char == MM:
+            send_command('MM')
+        elif key_char == center_turret:
+            send_command('center_turret')
+
+        simulate_key_press_release(key_char)
+
+# Crea un listener para el teclado
+keyboard_listener = pynput_keyboard.Listener(on_press=on_press)
+keyboard_listener.start()
+
+keyboard_listener.join()
