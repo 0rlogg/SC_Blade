9a2626b04cb1fe1603e00d0d993068a823a0b52b
Daniel Miranda Barcelona <73300841+0rlogg@users.noreply.github.com>
2024-08-16 21:16:15 +0200

    UPDATE

--- PC_BLADE.py
+++ PC_BLADE.py
@@ -1,40 +1,37 @@
 import socket
 import time
 import pyautogui  
-    
 
-# Configure the IP address and port
+# Configura la dirección IP y el puerto
 HOSTNAME = socket.gethostname()
-HOST =  input('type the ip of the pc-server')
+HOST = input('Type the IP of the PC-server: ')  # Asegúrate de ingresar la IP correcta aquí
 PORT = 61000
 
-print (HOSTNAME)
-print (HOST)
-print (PORT)
+print(HOSTNAME)
+print(HOST)
+print(PORT)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_info:
     socket_info.bind((HOST, PORT))
     socket_info.listen()
-    print(f"Waiting conexions in {HOST}:{PORT}")
+    print(f"Waiting for connections at {HOST}:{PORT}")
 
     while True:
         try:
             conn, addr = socket_info.accept()
-            print('Conexion from', addr)
+            print('Connected by', addr)
 
             while True:
                 command = conn.recv(1024).decode('utf-8')
                 if not command:
                     break
-                print(f"Comando recibido: {command}")
+                print(f"Command received: {command}")
 
-                # Ejecuta el comando recibido (simular la pulsación de tecla 'I')
+                # Ejecuta el comando recibido
                 if command == 'encendido':
                     pyautogui.press('e')
-                # Ejecuta el comando recibido (simular la pulsación de tecla 'I')
-                if command == 'target':
+                elif command == 'target':
                     pyautogui.press('t')
 
-
         except Exception as error:
-            print(f"Error de conexión: {error}")
+            print(f"Connection error: {error}")
--- PC_CLIENT.py
+++ PC_CLIENT.py
@@ -5,35 +5,39 @@ from pynput import keyboard as pynput_keyboard
 SERVER_IP = '192.168.1.242'  # Reemplaza con la dirección IP del PC secundario
 SERVER_PORT = 61000  # Elige un puerto disponible
 
-ALLOWED_KEYS = {'e', '0', 't', '1', '5'}  # Lista de teclas permitidas
-
-def send_command(command):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_INFO:
-        socket_INFO.connect((SERVER_IP, SERVER_PORT))
-        socket_INFO.sendall(command.encode('utf-8'))
-
-# Variables de estado para detectar Shift + e
+# Variable de estado para controlar el envío de comandos
+command_sending_enabled = False
 shift_pressed = False
 
+def send_command(command):
+    if command_sending_enabled:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_INFO:
+            socket_INFO.connect((SERVER_IP, SERVER_PORT))
+            socket_INFO.sendall(command.encode('utf-8'))
+            print(f"Comando enviado: {command}")
+
 def on_press(key):
-    global shift_pressed
+    global command_sending_enabled, shift_pressed
 
     try:
         if key == pynput_keyboard.Key.shift:
             shift_pressed = True
         elif key.char == 'e' and shift_pressed:
             send_command('encendido')
+        elif key.char == '.':
+            command_sending_enabled = not command_sending_enabled
+            state = "activado" if command_sending_enabled else "desactivado"
+            print(f"El envío de comandos ha sido {state}.")
     except AttributeError:
         pass
 
 def on_release(key):
     global shift_pressed
-
     if key == pynput_keyboard.Key.shift:
         shift_pressed = False
 
 # Listener para el teclado
-keyboard_listener = pynput_keyboard.Listener(on_press=on_press)
+keyboard_listener = pynput_keyboard.Listener(on_press=on_press, on_release=on_release)
 keyboard_listener.start()
 
 keyboard_listener.join()
